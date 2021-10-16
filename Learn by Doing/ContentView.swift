cc3530ad6264c02e61afb500ee9f08144dabc788
Mohmed Anis Agwan <anishagwan123@gmail.com>
2021-10-16 19:09:26 +0530

    Created the card view of developers

--- Learn by Doing/ContentView.swift
+++ Learn by Doing/ContentView.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK:- Content
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        CardView()
     }
 }
 
