f5cf0b3d3be574bdb403cbec44149cfdf0346489
Anis <anishagwan123@gmail.com>
2021-11-04 21:26:35 +0530

    Added UI Alert on button tap

--- Learn by Doing/CardView.swift
+++ Learn by Doing/CardView.swift
@@ -14,6 +14,7 @@ struct CardView: View {
     @State private var fadeIn: Bool = false
     @State private var moveDown: Bool = false
     @State private var moveUp: Bool = false
+    @State private var showAlert: Bool = false
     
     var hapticImpact = UIImpactFeedbackGenerator(style: .heavy)
     
@@ -42,6 +43,7 @@ struct CardView: View {
                 print("Button was pressed")
                 playSound(sound: "sound-chime", type: "mp3")
                 self.hapticImpact.impactOccurred()
+                self.showAlert.toggle()
             }, label: {
                 HStack {
                     Text(card.callToAction.uppercased())
@@ -74,6 +76,13 @@ struct CardView: View {
                 self.moveDown.toggle()
                 self.moveUp.toggle()
             }
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text(card.title),
+                message: Text(card.message),
+                dismissButton: .default(Text("OK"))
+            )
         }
     }
 }
